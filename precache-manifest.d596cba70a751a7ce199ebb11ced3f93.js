38fb7dca893b2733e655805524542a6144f08b4c
unknown <thekoolega123@gmail.com>
2020-05-28 21:04:51 +0200

    Updates

---
+++ precache-manifest.d596cba70a751a7ce199ebb11ced3f93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d0321b3e0b37edbb214980d5cb9e9e",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "5e72420594c93155d813",
+    "url": "/WeatherApp/static/js/2.c925a82d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WeatherApp/static/js/2.c925a82d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c66e05aa9b7f50188c8",
+    "url": "/WeatherApp/static/js/main.3306ca50.chunk.js"
+  },
+  {
+    "revision": "772a38e38779b7ab93e5",
+    "url": "/WeatherApp/static/js/runtime-main.ecf4bed7.js"
+  }
+]);
